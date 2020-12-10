ee18f70c613fc7f8ef1bcc62fcc9f73a88ca856c
codingkudos <75491359+codingkudos@users.noreply.github.com>
2020-12-10 17:16:50 +0000

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -2,7 +2,6 @@ package com.codingkudos.myapplication;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -19,17 +18,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        View coordinatorLayout = findViewById(R.id.coordinatorLayout);
+        View coordinatorLayout = findViewById(R.id.coodinatorLayout);
 
         TextView textToBeDeleted = findViewById(R.id.textToBeDeleted);
 
-        Button buttonSnackbar = findViewById(R.id.buttonDeleteText);
-        buttonSnackbar.setOnClickListener(new View.OnClickListener() {
+        Button buttonDeleteText = findViewById(R.id.buttonDeleteText);
+        buttonDeleteText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(TAG, "onClick: clicked");
                 textToBeDeleted.setText("");
-                Snackbar snackbar = Snackbar.make(coordinatorLayout, "Button is clicked", Snackbar.LENGTH_INDEFINITE)
+                Snackbar snackbar = Snackbar.make(coordinatorLayout, "Text deleted", Snackbar.LENGTH_INDEFINITE)
                         .setAction("UNDO", new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
